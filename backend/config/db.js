6d2a10a19fc673bf3b6a5c397a0b626691007908
Faraz Sarfaraz Agha <fagha4697@gmail.com>
2026-04-30 16:08:52 +0530

    all errors are fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,24 @@
-const mongoose = require("mongoose");
+/*const mongoose = require("mongoose");
 
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI);
   console.log(`MongoDB connected: ${conn.connection.host}`);
   console.log("DB Name:", mongoose.connection.name);
+};
+
+module.exports = connectDB; */
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1); // ❌ crash karta hai
+  }
 };
 
 module.exports = connectDB;
